102d2f4a481e8007a08c5d044df7fbf0908dd793
Prometheus <blackjack66613@gmail.com>
2023-02-15 18:35:06 +0300

    Выполнение условий первым способом

--- Program.cs
+++ Program.cs
@@ -22,9 +22,26 @@ void ShowArray(string[] arrayString)
     Console.Write("]");    
 }
 
+void ArrString(string[] arrayString, int size)
+{ 
+  Console.Write("[");
+  for (int i = 0; i < size; i++)
+  {
+      if (arrayString[i].Length <= 3)
+      {
+       Console.Write(arrayString[i] + " ");
+      }
+    
+  }
+  Console.Write("]");
+}
+
 Console.Write("Input a array size: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input a array string: ");
 
 string[] MyArray = ArrayString(m);
 ShowArray(MyArray);
+Console.WriteLine();
+Console.WriteLine();
+ArrString(MyArray, m);
