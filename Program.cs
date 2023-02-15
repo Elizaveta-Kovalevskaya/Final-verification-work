3a7ef881ad73c603c9c822ac76653b6d30b06306
Prometheus <blackjack66613@gmail.com>
2023-02-15 16:47:06 +0300

    Создание одномерного массива

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] ArrayString (int size)
+{
+    string[] arrayString = new string[size]; 
+    
+    
+    for(int i = 0; i < size; i++)
+    {
+     
+     arrayString[i] = Convert.ToString(Console.ReadLine());
+     
+    }
+    return arrayString;
+}
